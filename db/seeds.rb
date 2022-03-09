00eb05c10c6a130a294036643cd1d950bf1f16ff
Will Grund <willgrund178@gmail.com>
2022-03-09 12:39:49 -0500

    Creates toaster product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create(name: "Toaster", price: 39, image_url: "abc", description: "Toasts bread")
